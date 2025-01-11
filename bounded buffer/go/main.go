a853a05f20ae7d33f82d11af15a96c7770de41f5
mariaalida <maria.a.h.olsen@ntnu.no>
2025-01-11 15:20:09 +0100

    task5

---
+++ bounded buffer/go/main.go
@@ -0,0 +1,40 @@
+
+package main
+
+import "fmt"
+import "time"
+
+
+func producer(buf chan int){
+
+    for i := 0; i < 10; i++ {
+        time.Sleep(100 * time.Millisecond)
+        fmt.Printf("[producer]: pushing %d\n", i)
+        // TODO: push real value to buffer
+        buf<-i
+    }
+    close(buf)
+}
+
+func consumer(buf chan int){
+
+    time.Sleep(1 * time.Second)
+    for {
+        i := <-buf //TODO: get real value from buffer
+        fmt.Printf("[consumer]: %d\n", i)
+        time.Sleep(50 * time.Millisecond)
+    }
+    
+}
+
+
+func main(){
+    
+    // TODO: make a bounded buffer
+    buf:=make(chan int, 5)
+
+    go consumer(buf)
+    go producer(buf)
+    
+    select {}
+}
--- result.md
+++ result.md
@@ -1,2 +1,6 @@
 Both the incrementing and the decrementing function shares a global variable. When running the functions, they run at the same time but are not communicating. This means that they may retrive the value of i at the same time and when i is refreshed, it is not both increased and decreased. 
 
+task4: 
+I used pthread_mutex to make sure only one thread is active at the time since they share a value
+
+
